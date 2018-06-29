f403a621100ad90093e08515e964c01bbbe2333c
vichaan <40622307+vichaan@users.noreply.github.com>
2018-06-29 15:17:27 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = 'Cn9UvRdGKcXk4eFXca50WLpxNtvoyWBaGfs7RwWqt5p';
+$access_token = '+j6rBY6eMV5j4EFGoPc7L+Jm6AqYcCTwU1vi3v0PYX/TwDR41Kd2ERctXwgXpqXGzsLPZOiSwQFjUXF7pllmLSm5Fvf5SNU5AVUaqvmOEe+0PWAHdw9QA3JPhNh8jLYRZFxHh7GQlmctFa8lnXeq4gdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
